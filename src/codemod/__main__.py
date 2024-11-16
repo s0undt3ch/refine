3739b1f0f7cbc9004cea4f6eaa70517b427b3de2
Pedro Algarvio <pedro@algarvio.me>
2024-11-16 13:07:19 +0000

    A working CLI entry point!

--- src/codemod/__main__.py
+++ src/codemod/__main__.py
@@ -1,3 +1,121 @@
 """
-CLI entru-point for codemod.
+Utility to simplify rewriting python code.
+
+It can be used for one-off rewrites, or, to maintain code styling rules.
 """
+
+import argparse
+import importlib.metadata
+import logging
+import pathlib
+import sys
+from multiprocessing import freeze_support
+
+from codemod.config import Config
+from codemod.processor import Processor
+from codemod.registry import Registry
+
+try:
+    VERSION = importlib.metadata.version("codemod")
+except importlib.metadata.PackageNotFoundError:
+    VERSION = "0.0.0.not-installed"
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr, format="%(message)s")
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--version", action="version", version=VERSION)
+    parser.add_argument(
+        "files", metavar="FILE", nargs="+", type=pathlib.Path, help="One or more files to process."
+    )
+    parser.add_argument(
+        "--config",
+        type=pathlib.Path,
+        help="Path to config file. Defaults to '.codemod.ini' on the current directory.",
+        default=".codemod.toml",
+    )
+    parser.add_argument(
+        "--quiet", "-q", action="store_true", default=False, help="Quiet down the tool output."
+    )
+    parser.add_argument(
+        "--list-codemods",
+        "--list",
+        action="store_true",
+        help="List all available codemods",
+    )
+    parser.add_argument(
+        "--exclude-codemod",
+        "--exclude",
+        default=[],
+        action="append",
+        help="Exclude codemods from available codemods.",
+    )
+    parser.add_argument(
+        "--select-codemod",
+        "--select",
+        default=[],
+        action="append",
+        help="Explicitly select codemod names from available codemods.",
+    )
+    parser.add_argument(
+        "--codemods-path",
+        type=pathlib.Path,
+        action="append",
+        default=[],
+        dest="codemods_paths",
+        help="Path to a codemods directory. Can be passed multiple times.",
+    )
+    args = parser.parse_args()
+    if args.quiet:
+        logging.getLogger().setLevel(logging.ERROR)
+    if not args.config.is_absolute():
+        config_file = pathlib.Path.cwd().joinpath(args.config).resolve()
+    else:
+        config_file = args.config
+    if config_file.exists():
+        if config_file.name == "pyproject.toml":
+            config = Config.from_pyproject_file(config_file)
+        else:
+            config = Config.from_default_file(config_file)
+    elif pathlib.Path.cwd().joinpath("pyproject.toml").exists():
+        config = Config.from_toml(pathlib.Path.cwd().joinpath("pyproject.toml"))
+    else:
+        config = Config()
+
+    if args.list_codemods:
+        # Add the additional CLI passed codemod paths
+        config.codemod_paths.update(set(args.codemods_paths))
+
+    if args.select_codemod:
+        # Add any additional CLI passed selections
+        config.select.update(set(args.select_codemod))
+
+    if args.exclude_codemod:
+        # Add any additional CLI passed exclusions
+        config.exclude.update(set(args.exclude_codemod))
+
+    registry = Registry()
+    registry.load(config.codemod_paths)
+    if args.list_codemods:
+        log.info("Available codemods:")
+        for codemod in registry.codemods():
+            log.info(" - %s: %s", codemod.NAME, codemod.DESCRIPTION)
+        parser.exit()
+
+    codemods = list(
+        registry.codemods(select_codemods=config.select, exclude_codemods=config.exclude)
+    )
+    log.info("Selected codemods:")
+    for codemod in codemods:
+        log.info(" - %s: %s", codemod.NAME, codemod.DESCRIPTION)
+
+    processor = Processor(config=config.freeze(), registry=registry, codemods=codemods)
+    processor.process(args.files)
+
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
